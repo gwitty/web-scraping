c07fb1bd2218a1dcde39f43c014c8637971fe935
George Witty <george@supapass.com>
2019-08-16 13:01:47 +0100

    Update drip.py to be more readable

--- drip/drip.py
+++ drip/drip.py
@@ -5,49 +5,48 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 
-# source_code = requests.get('http://fantasy.espn.com/basketball/league/standings?leagueId=633975')
-
+# Random option stuff
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 capa = DesiredCapabilities.CHROME
 capa["pageLoadStrategy"] = "none"
-# driver = webdriver.Chrome(chrome_options=options, desired_capabilities=capa)
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# Open window
 driver.set_window_size(1440,900)
+
+# Go to drip website
 driver.get('http://d.rip/discover')
 time.sleep(5)
 
-#plain_text = driver.page_source
-#soup = BeautifulSoup(plain_text, 'lxml')
-
-#soup.select('.Table2__header-row') # Returns full results.
-
-#len(soup.select('.Table2__header-row')) # 8
-
+# Click the "Show more" button to get the entire list of creators
 for i in range(11):
     button = driver.find_elements_by_tag_name('button')[1]
     button.click()
     time.sleep(3)
 
-#num_artists = len(driver.find_elements_by_class_name('text-truncate'))
-
-#print(num_artists)
-
+# Get the info for the site
 soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+# Find the artist names and put then in an array
 artist_soup = soup.find_all('div', attrs={'class': 'text-truncate'})
 artist_array = [i.getText() for i in artist_soup]
 
+# Find the genres and put them in an array
 genre_soup = soup.find_all('div', attrs={'class': 'pitch-bold type-14 text-uppercase h4 drip-light-gray'})
 genre_array = [i.getText() for i in genre_soup]
 
+# Find the number of subscribers and put them in an array
 subs_soup = soup.find_all('p', attrs={'class': 'drip-light-gray'})
 subs_array = [i.getText() for i in subs_soup]
 
+# Smash all the arrays together
 big_array = [artist_array, genre_array, subs_array]
 
+# Transpose the array because I've done it the wrong way round
 transposed_array = zip(*big_array)
 
-####
+# Now create a csv
 
 import csv
 
